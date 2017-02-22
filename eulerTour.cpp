93cf35d9747952dfc001a4be3bd18b411f40a860
Dayana Rodrigues <dayana2301@gmail.com>
2017-02-22 16:16:33 -0400

    euler tour

---
+++ eulerTour.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <cstdio>
+#include <utility>
+//#include <iostream.h>
+#include "graph.cpp"
+
+using namespace std;
+
+// determinate if a edge can be part of the euler tour
+bool validEdge(Graph graph, Node node, Edge edge){
+	if(node.edges.size() == 1){
+		return true;
+	}
+	bool visited[graph.nodes.size()];
+	int dfsc1 = countNodes(node, visited);
+	node = removeEdge(node, edge);
+	int dfsc2 = countNodes(node, visited);
+	node = addEdge(node, edge);
+	if(dfsc1 > dfsc2){
+		return false;
+	}
+	return true;
+}
+
+// count the reachable nodes fron a node
+int countNodes(Node node, bool visited[]){
+	visited[node.node] = true;
+	int r_nodes = 1:
+
+	for(int i=0; i<node.edges.size(); i++){
+		if(!visited[node.edges[i].s_node]){
+			r_nodes += countNodes(node.edges[i].s_node, visited);
+		}
+	}
+	return r_nodes;
+}
+
+Graph eulerTour(Node node){
+
+	Node aux_node;
+
+	for(int i=0; i<node.edges.size(); i++){
+		Edge c_edge = node.edges[i];
+		if(validEdge(graph, deposit, c_edge)){
+			aux_node.node = node.node;
+			aux_node.edges.push_back(c_edge);
+			//new_graph = new_graph.nodes.push_back(aux_node);
+			cout << node << "-" << c_edge.s_node;
+			eulerTour(c_edge.s_node);
+		}
+	}
+	return new_graph;
+}
