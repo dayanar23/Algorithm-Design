afa51263e5f3b30d80d1ca3d01c76d4344f9bea0
Dayana Rodrigues <dayana2301@gmail.com>
2017-02-26 21:35:19 -0400

    seleccion de nodo final

--- max_benefit.cpp
+++ max_benefit.cpp
@@ -8,16 +8,16 @@ using namespace std;
 
   
 // return the node who has the minimun distance
-int minDistance(int dist[], vector<bool> sptSet, Graph g){
+int minDistance(int dist[], bool sptSet[], Graph g){
   // Initialize min value
   int min = INT_MIN, min_index;
   
   // find the node with min distance
-  for (int v = 0; v < g.vertex; v++)
+  for (int v = 0; v < g.vertex; v++){
     if (sptSet[v] == false && dist[v] >= min){
       min = dist[v], min_index = v;
     }
-  
+  }
   return min_index;
 }
 
@@ -40,6 +40,7 @@ int minDistance2(int dist[], bool sptSet[],Graph g){
 void printEdges(vector<Edge> edges){
   for(int i =0; i<edges.size(); i++){
     cout << endl << edges[i].n1 << "-" << edges[i].n2 << endl;
+    cout << "c: " << edges[i].cost << " b: " << edges[i].benef << endl;
   }
 }
 
@@ -47,7 +48,7 @@ void printEdges(vector<Edge> edges){
 vector<Edge> filterEdges(vector<Edge> old, Graph g){
 
   // says if the vertex already was on the new solution
-  bool ver[g.vertex];
+  bool ver[g.vertex+1];
   // initialize
   for (int i = 0; i < g.vertex; i++){
     ver[i] = false;
@@ -88,17 +89,19 @@ int getDistance(int dist[], int node){
 // Funtion that based on dijkstra algorithm for search
 // the min cost path from deposit vertex to the others
 // and return the max benefit
-int firstPath(Graph g, int deposit){
+int *firstPath(Graph g, int deposit){
 
-  int dist[g.vertex]; // distance from deposit  to i
+  int dist[g.vertex+1]; // distance from deposit  to i
 
   // vetor with the shortest path.
   vector<Edge> vec;
   
+  bool stpSet[g.vertex+1];
   // initialize distances with the min value
   // and the vector visited of the graph with false
-  for (int i = 0; i < g.vertex; i++)
-        dist[i] = INT_MIN, g.visited[i] = false;
+  for (int i = 0; i < g.vertex+1; i++){
+    dist[i] = INT_MIN, stpSet[i] = false;
+  }
   
   // distance of deposit from itself is always 0
   dist[deposit] = 0;
@@ -108,22 +111,18 @@ int firstPath(Graph g, int deposit){
 
     // choose the vertex withminimum distance from the set 
     // of vertices not visited.
-    int u = minDistance(dist, g.visited, g);
-
+    int u = minDistance(dist, stpSet, g);
     // Mark the vertex as visited
-    g.visited[u] = true;
-  
+    stpSet[u]= true;
     // update dist value of the adjacent vertices of the u vertex
-    for (int v = 0; v < g.vertex; v++){
-  
-         // Update dist[v] only if is not visited
-      if (!g.visited[v] && g.edgeExist(u,v) && dist[u] != INT_MIN
-              && dist[u]+ g.getTotalBenefit(u,v) > dist[v]){
+    for (int v = 2; v < g.vertex+1; v++){
 
+         // Update dist[v] only if is not visited
+      if (!stpSet[v] && g.edgeExist(u,v) && dist[u] != INT_MIN
+              && dist[u]+ g.getTotalBenefit(u,v) > dist[v]){
         // update distance and mark de edge as crossed
         dist[v] = dist[u] + g.getTotalBenefit(u,v);
         g.setCrossed(u,v);
-
         // insert value into the solution vector
         vec.push_back(g.getEdge(u,v));
       }
@@ -131,7 +130,7 @@ int firstPath(Graph g, int deposit){
   }
   
   // return the cost of the min path
-  return getDistance(dist, g.vertex-1);
+  return dist;
 }
  
 // Funtion that based on dijkstra algorithm for search
@@ -139,17 +138,17 @@ int firstPath(Graph g, int deposit){
 // and return the max benefit
 int secondPath(Graph g, int last_v){
 
-    int dist[g.vertex];  // distance from deposit  to i
+    int dist[g.vertex+1];  // distance from deposit  to i
 
     // vetor with the shortest path.
     std::vector<Edge> vec;
 
     // array who say if a vertex was visited.
-    bool stpSet[g.vertex];
+    bool stpSet[g.vertex+1];
 
     // initialize distances with the min value
     // and the vector vec with false
-    for (int i = 0; i < g.vertex; i++)
+    for (int i = 0; i < g.vertex+1; i++)
       dist[i] = INT_MIN, stpSet[i] = false;
   
     // distance of a vertex from itself is always 0
@@ -161,12 +160,11 @@ int secondPath(Graph g, int last_v){
       // choose the vertex withminimum distance from the set 
       // of vertices not visited.
       int u = minDistance2(dist, stpSet, g);
-
       // mark u as visited
       stpSet[u] = true;
   
        // update dist value of the adjacent vertices of the u vertex
-      for (int v = 0; v < g.vertex; v++){
+      for (int v = 1; v< g.vertex+1; v++){
   
         // Update dist[v] only if is not visited
         if (!stpSet[v] && g.edgeExist(u,v) && dist[u] != INT_MIN
@@ -175,7 +173,6 @@ int secondPath(Graph g, int last_v){
           // update distance and mark de edge as crossed
           dist[v] = dist[u] + g.getTotalBenefit(u,v);
           g.setCrossed(u,v);
-
           // insert value into the solution vector
           vec.push_back(g.getEdge(u,v));
         }
@@ -183,9 +180,17 @@ int secondPath(Graph g, int last_v){
     }
   
     // return the cost of the min path
-    return getDistance(dist, 0);
+    return getDistance(dist, 1);
 }
 
+int minVertex(int dist[], int v){
+  for (int i=v; i > 0; i--){
+      cout << i << endl;
+    if(dist[i] < 1200000000 && dist[i] > -1200000000){
+      return i;
+    }
+  }
+}
 
 int main(int argc, char **argv) {
 
@@ -194,61 +199,16 @@ int main(int argc, char **argv) {
 
   filename = argv[1];
 
+  // build the graph with the instance info
   graph = buildGraph(filename);
 
-  printEdges(graph->a_list);
+  //calculate the max benefit path
+  int *a = firstPath(*graph,1);
+  int v = minVertex(a, graph->vertex);
+  int b = secondPath(*graph,v);
 
-  cout << firstPath(*graph,0) + secondPath(*graph,5) << endl;
-
- /* 
-  Edge e1(0,1,2,10);
-  Edge e2(0,2,10,0);
-  Edge e3(1,2,3,2);
-  Edge e4(1,3,20,5);
-  Edge e5(3,5,9,1);
-  Edge e6(1,4,1,3);
-  Edge e7(2,4,5,4);
-  Edge e8(2,3,3,4);
-  Edge e9(4,3,2,8);
-  Edge e10(4,5,8,1);
-
-  Edge e11(1,0,2,10);
-  Edge e12(2,0,10,0);
-  Edge e13(2,1,3,2);
-  Edge e14(3,1,20,5);
-  Edge e15(5,3,9,1);
-  Edge e16(4,1,1,3);
-  Edge e17(4,2,5,4);
-  Edge e18(3,2,3,4);
-  Edge e19(3,4,2,8);
-  Edge e20(5,4,8,1);
-
-  std::vector<Edge> v;
-
-  v.push_back(e1);
-  v.push_back(e2);
-  v.push_back(e3);
-  v.push_back(e4);
-  v.push_back(e5);
-  v.push_back(e6);
-  v.push_back(e7);
-  v.push_back(e8);
-  v.push_back(e9);
-  v.push_back(e10);
-  v.push_back(e11);
-  v.push_back(e12);
-  v.push_back(e13);
-  v.push_back(e14);
-  v.push_back(e15);
-  v.push_back(e16);
-  v.push_back(e17);
-  v.push_back(e18);
-  v.push_back(e19);
-  v.push_back(e20);
-
-  Graph g1(6,20,v);
-
-  cout << firstPath(g1,0) + secondPath(g1,5) << endl;*/
+  // return the Vo value
+  cout << v+b << endl;
   
   return 0;
 }
